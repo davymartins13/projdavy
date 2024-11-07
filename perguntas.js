3f1fe8449b4d0edb0073d7f8e883dded9605de86
davymartins13 <00001216829251sp@al.educacao.sp.gov.br>
2024-11-07 10:27:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes',
+    'Interestelar',
+    'Filme de 2014 sobre ficção científica digirido por Christopher Nolan'
+)
+
+criaCartao(
+    'Séries',
+    'Stranger Things',
+    'Passado nos anos 80, grupo adolescente presencia seres sobrenaturais em uma pequena cidade de Indiana'
+)
+
+criaCartao(
+    'Livro',
+    'Melhor do que nos Filmes',
+    'Para fães de comédia romântica, o livro retrata a história de um amor na adolescência'
+)
+
+criaCartao(
+    'Música',
+    'Mirrorball',
+    'Escrita por Taylor Swift, nela descreve as diferentes versões de uma pessoa'
+)
